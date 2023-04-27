c3a9d2663d4fed07a13f6cb259bf18a7b4370efa
vedant <43410160+rvedant5@users.noreply.github.com>
2023-04-27 17:33:01 +0530

    nav and text form added

---
+++ src/components/TextForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function TextForm() {
+  return (
+
+      <div className="mb-3">
+        <label for="myBox" class="form-label">Type Here:</label>
+        <textarea className="form-control" id="myBox" rows="3"></textarea>
+      </div>
+
+  );
+}                 
