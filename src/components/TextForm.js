2a4071e7eeb725a453ed68f8a9d037fc6d70ed4f
vedant <43410160+rvedant5@users.noreply.github.com>
2023-05-08 11:01:21 +0530

    Completed textform

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,19 +3,35 @@ import React, {useState} from "react";
 
 export default function TextForm(props) {
   const handleUPClick = ()=>{
-    setText("You have clicked on Upper Case")
+    let newText=text.toUpperCase();
+    setText(newText)
+  }
+  const handleLowClick = ()=>{
+    let newText=text.toLowerCase();
+    setText(newText)
   }
   const handleOnChange = (event)=>{
     setText(event.target.value);
   }
-  const [text,setText]=useState('Enter text here'); 
+  const [text,setText]=useState(); 
   return (
-      <div>
+    <>
+      <div className="container" >
         <h1>{props.heading}</h1>
         <div className="mb-3">
           <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="5"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUPClick}>Upper Case</button>
+        <button className="btn btn-primary mx-1" onClick={handleUPClick}>Upper Case</button>
+        <button className="btn btn-primary mx-1" onClick={handleLowClick}>Lower Case</button>
+
       </div>
+      <div className="container summary">
+        <h1>Text Summary: </h1>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <h2>Preview:</h2>
+        <p>{text}</p>
+      </div>
+    </>
+      
   );
 }                 
