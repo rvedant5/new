2e17a02f38514a008c464f63888119456989b8f4
vedant <43410160+rvedant5@users.noreply.github.com>
2023-05-08 11:34:01 +0530

    Completed version

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import TextForm from "./components/TextForm";
 function App() {
   return (
     <>
-      <Navbar title="TextUtils"/>
-      
-      <div className="container my-3"><TextForm heading="Enter the text here to analyze"/></div>
+      <Navbar title="TextUtils" />
+
+      <div className="container my-3">
+        <TextForm heading="Enter the text here to analyze" />
+      </div>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,37 +1,48 @@
-import React, {useState} from "react";
-
+import React, { useState } from "react";
 
 export default function TextForm(props) {
-  const handleUPClick = ()=>{
-    let newText=text.toUpperCase();
-    setText(newText)
-  }
-  const handleLowClick = ()=>{
-    let newText=text.toLowerCase();
-    setText(newText)
-  }
-  const handleOnChange = (event)=>{
+  const handleUPClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleLowClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+  const handleOnChange = (event) => {
     setText(event.target.value);
-  }
-  const [text,setText]=useState(); 
+  };
+
+  const [text, setText] = useState("");
+
   return (
     <>
-      <div className="container" >
+      <div className="container">
         <h1>{props.heading}</h1>
         <div className="mb-3">
-          <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="5"></textarea>
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="5"
+          ></textarea>
         </div>
-        <button className="btn btn-primary mx-1" onClick={handleUPClick}>Upper Case</button>
-        <button className="btn btn-primary mx-1" onClick={handleLowClick}>Lower Case</button>
-
+        <button className="btn btn-primary mx-1" onClick={handleUPClick}>
+          Upper Case
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleLowClick}>
+          Lower Case
+        </button>
       </div>
       <div className="container summary">
         <h1>Text Summary: </h1>
-        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
         <h2>Preview:</h2>
         <p>{text}</p>
       </div>
     </>
-      
   );
-}                 
+}
